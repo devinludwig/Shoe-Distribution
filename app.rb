327e6b18c023f757d3abac10088062e350fdea70
Devin Ludwig <devinludwig@gmail.com>
2016-12-16 09:14:48 -0800

    add index.erb content and routing to list and add shoe stores.

--- app.rb
+++ app.rb
@@ -7,3 +7,13 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+post('/store') do
+  Store.create({:name => params.fetch('store')})
+  redirect to('/')
+end
+
+get('/store/:id') do
+  @store = Store.find(params.fetch('id'))
+  erb(:store)
+end
